5d2ceec2bcb4b538d4a17f609b0de4dc8097d9e9
Rayane Araújo <rayanearaujo28@hotmail.com>
2024-07-09 15:04:03 -0400

    feat:armazenando informação da pagina do usuário

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -22,51 +22,85 @@ interface Attendee {
 
 
 export function AttendeeList() {
-    const [search, setSearch ] = useState('')
-    const [page, setPage] = useState(1)
+    const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString());
 
-    const [total, setTotal] = useState(0)
-    const [attendees, setAttendees] = useState<Attendee[]>([])
-
-    const totalPages = Math.ceil(total / 10)
-
-    useEffect(() => {
-        const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
-
-        url.searchParams.set('pageIndex', String(page - 1))
-
-        if(search.length > 0){
-          url.searchParams.set('query', search)
+        if (url.searchParams.has("search")) {
+            return url.searchParams.get("search") ?? "";
         }
 
+        return "";
+    });
+
+    const [page, setPage ] = useState(() => {
+    const url = new URL(window.location.toString());
+
+        if (url.searchParams.has("page")) {
+            return Number(url.searchParams.get("page"));
+        }
+
+        return 1;
+    });
+
+    const [total, setTotal] = useState(0);
+    const [attendees, setAttendees] = useState<Attendee[]>([]);
+
+    const totalPages = Math.ceil(total / 10);
+
+    useEffect(() => {
+        const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees');
+
+        url.searchParams.set('pageIndex', String(page - 1));
+        url.searchParams.set('query', search);
+
         fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            setAttendees(data.attendees)
-            setTotal(data.total)
-        })
-    }, [page, search])
+        .then((response) => response.json())
+        .then((data) => {
+            setAttendees(data.attendees);
+            setTotal(data.total);
+        });
+    }, [page, search]);
+
+
+    function setCurrentSearch(search: string) {
+        const url = new URL(window.location.toString());
+    
+        url.searchParams.set("search", search);
+    
+        window.history.pushState({}, "", url);
+    
+        setSearch(search);
+    }
+
+    function setCurrentPage(page: number) {
+        const url = new URL(window.location.toString());
+    
+        url.searchParams.set("page", String(page));
+    
+        window.history.pushState({}, "", url);
+    
+        setPage(page);
+    }
 
     function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-        setSearch(event.target.value)
-        setPage(1)
+        setCurrentSearch(event.target.value);
+        setCurrentPage(1);
     }
 
     function goToNextPage() {
-        setPage(page + 1)
+        setCurrentPage(page + 1);
     }
 
     function goToPreviousPage() {
-        setPage(page - 1)
+        setCurrentPage(page - 1);
     }
 
     function goToFirstPage() {
-        setPage(1)
+        setCurrentPage(1);
     }
 
     function goToLastPage() {
-        setPage(totalPages)
+        setCurrentPage(totalPages);
     }
 
     return (
@@ -75,8 +109,10 @@ export function AttendeeList() {
                 <h1 className='text-2xl font-bold'>Participantes</h1>
                 <div className="px-3 py-1.5 w-72 border border-white/10 bg-transparent rounded-lg text-sm flex items-center gap-3">
                     <Search className='size-4' />
-                    <input onChange={onSearchInputChanged} 
-                    className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" placeholder="Buscar participantes..." />
+                    <input 
+                    onChange={onSearchInputChanged} 
+                    className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" placeholder="Buscar participantes..."
+                    value={search}/>
                 </div>
             
             </div>
